6c9e8b13ca873275cb333ffcd7fd8e2104e2339a
Zach Turner <107499062+zachtu@users.noreply.github.com>
2023-10-22 22:23:21 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Zach on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
