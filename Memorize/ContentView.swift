e3d634cd8ab7680295e16add931814af1ef008b8
Zach Turner <107499062+zachtu@users.noreply.github.com>
2023-10-29 11:11:57 +0000

    Remove adjuster buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,56 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🌻", "🐛", "🧽", "🐌", "🌻", "👽", "🧽", "🐌"]
-    @State var cardCount = 4
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button {
-            cardCount += offset
-            
-        } label: {
-            Image(systemName: symbol)
-        }
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
+    let emojis = ["🌻", "🐛", "🧽", "🐌", "👽"]
     
     var body: some View {
         VStack {
             ScrollView {
                 cards
             }
-            Spacer()
-            cardCountAdjusters
         }
         .padding()
     }
     
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(withEmoji: emojis[index])
+            ForEach(emojis, id: \.self) { emoji in
+                CardView(withEmoji: emoji)
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
     }
 }
 
